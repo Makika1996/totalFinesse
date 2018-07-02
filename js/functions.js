2288708a134f3b497ce9f0f8d47d8de941471516
Marija Marinkov <marijamarinkov96@gmail.com>
2018-07-02 11:19:06 +0200

    v1.2 buttons

--- js/functions.js
+++ js/functions.js
@@ -22,12 +22,55 @@ $(function() {
       arrows: false
     });
 
-    // $(".page-slider").slick({
-    //   slidesToShow: 1,
-    //   slidesToScroll: 1,
-    //   arrows: true,
-    //   vertical: true
-    // });
+    $(".btn-contact").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#contact-us").offset().top + "px"
+      }, 1000)
+    });
+
+    $(".btn-training").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#training").offset().top + "px"
+      }, 1000)
+    });
+
+    $(".btn-digital").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#digital").offset().top + "px"
+      }, 1000)
+    });
+
+    $(".btn-liveEvents").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#liveEvents").offset().top + "px"
+      }, 1000)
+    });
+    $(".btn-clients").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#clients").offset().top + "px"
+      }, 1000)
+    });
+
+    $(".btn-top").on("click", function() {
+      $("html, body").animate({
+        scrollTop: $("#finesse").offset().top + "px"
+      }, 1000)
+    });
+
+    $(".page-slider").onepage_scroll({
+      sectionContainer: "section",
+      easing: "ease",
+      animationTime: 1000,
+      pagination: true,
+      updateURL: false,
+      beforeMove: function(index) {},
+      afterMove: function(index) {},
+      loop: false,
+      keyboard: true,
+      responsiveFallback: false,
+      direction: "vertical"
+    });
+
   });
 
 
